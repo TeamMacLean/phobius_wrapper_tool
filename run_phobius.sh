9878efbcd60aaf780319f1a962d73ff4fd86d747
Ram Krishna Shrestha <ram_krishna.shrestha@tsl.ac.uk>
2019-08-22 17:00:43 +0100

    Update run_phobius.sh
    type error correction

--- run_phobius.sh
+++ run_phobius.sh
@@ -4,4 +4,4 @@ fasta_file=$1
 format=$2
 output_file=$3
 
-/tsl/software/testing/phobius/1.01/x86_64/bin/phobius.pl -${fomat} ${fasta_file} > ${output_file}
+/tsl/software/testing/phobius/1.01/x86_64/bin/phobius.pl -${format} ${fasta_file} > ${output_file}
