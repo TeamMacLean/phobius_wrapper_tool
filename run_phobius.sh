ed2e2497f885893afb81d865528c62bb962e5445
Ram Krishna Shrestha <ram_krishna.shrestha@tsl.ac.uk>
2019-08-22 16:55:54 +0100

    Update run_phobius.sh

--- run_phobius.sh
+++ run_phobius.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 fasta_file=$1
-output_file=$2
+format=$2
+output_file=$3
 
-/usr/bin/perl /tsl/software/testing/phobius/1.01/x86_64/bin/phobius.pl -long ${fasta_file} > ${output_file}
+/usr/bin/perl /tsl/software/testing/phobius/1.01/x86_64/bin/phobius.pl -${fomat} ${fasta_file} > ${output_file}
